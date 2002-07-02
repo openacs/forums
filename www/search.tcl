6ac1cafbcac5378ce47cbee421304b7501554e1d
Yonatan Feldman <yon@milliped.com>
2002-07-02 14:29:58 +0000

    remove special characters from search text so taht we don't crash interMedia

--- www/search.tcl
+++ www/search.tcl
@@ -28,6 +28,10 @@ element create search forum_id \
 if {[form is_valid search]} {
     form get_values search search_text forum_id
 
+    # remove any special characters from the search text so we
+    # don't crash interMedia
+    regsub -all {[^A-Za-z0-9]} $search_text {} search_text
+
     set query search_all_forums
     if {![empty_string_p $forum_id]} {
         set query search_one_forum
