c092f700c0e4a4eef1ec249f73b39e83ebe58e51
Tilmann Singer <tils@tils.net>
2003-09-22 20:46:04 +0000

    Adding a listing of emoticons in the docs.

--- tcl/messages-format-procs.tcl
+++ tcl/messages-format-procs.tcl
@@ -17,6 +17,9 @@ namespace eval forum::format {
         the img tag that gets constructed here.
     } {
         set emoticons_map [list]
+
+        # if you change this list, consider changing
+        # www/doc/emoticons.adp as well
         foreach { emoticon image } {
             ":-)" "smile" 
             ";-)" "wink"
