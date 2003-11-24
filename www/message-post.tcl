98dab15bdbb57d00d636fcc8ab47db2ed7535e6c
Peter Marklund <peter_marklund@fastmail.fm>
2003-11-24 17:57:55 +0000

    fixing the forums attachments bug (again), most of the debugging time caused by the misfeature that db_multirow clobbs variables by default

--- www/message-post.tcl
+++ www/message-post.tcl
@@ -109,21 +109,17 @@ element create message anonymous_p \
 set attachments_enabled_p [forum::attachments_enabled_p]
 
 if {$attachments_enabled_p} {
-    if { ![info exists attach_p] } {
-        set attach_p 0
-    }
-
     element create message attach_p \
             -label [_ forums.Attach] \
             -datatype text \
             -widget radio \
-            -value $attach_p \
+            -value 0 \
             -options [list [list [_ forums.No] 0] [list [_ forums.Yes] 1]]
 }
 
 if {[form is_valid message]} {
     form get_values message \
-        message_id forum_id parent_id subject content html_p confirm_p subscribe_p anonymous_p
+        message_id forum_id parent_id subject content html_p confirm_p subscribe_p anonymous_p attach_p
     
     if { !$anonymous_allowed_p } {
         set anonymous_p 0
@@ -136,6 +132,7 @@ if {[form is_valid message]} {
 	set subject.spellcheck ":nospell:"
 	set content.spellcheck ":nospell:"
         set content [string trimright $content]
+
         set exported_vars [export_form_vars message_id forum_id parent_id subject content html_p confirm_p subject.spellcheck content.spellcheck anonymous_p attach_p]
         
         set message(html_p) $html_p
@@ -266,4 +263,3 @@ if {![empty_string_p $parent_id]} {
 } else {
     lappend context [_ forums.Post_a_Message]
 }
-
