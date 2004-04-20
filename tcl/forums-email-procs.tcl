742e387c7047ff4a469e90d131590f5efdfb60bc
Jeff Davis <davis@xarg.net>
2004-04-20 21:13:42 +0000

    merge all the changes on the oacs-5-1 and dotlrn-2-0 branches up to tag jcd-merge-20040420

--- tcl/forums-email-procs.tcl
+++ tcl/forums-email-procs.tcl
@@ -20,14 +20,22 @@ ad_proc -public forum::email::create_forward_email {
     upvar $message_passed message
 
     # Variables for I18N message lookup:
-    set posting_date $message(posting_date)
+    set posting_date $message(posting_date_ansi)
     set user_name $message(user_name)
 
     # Set up the message body
     set new_body "[ad_html_to_text -- $pre_body]"
     append new_body "\n\n===================================\n\n"
-    append new_body "[_ forums.email_alert_body_header]\n\n"
-    append new_body "[ad_html_to_text -- $message(content)]\n"
+    append new_body "[_ forums.email_alert_body_header]
+[_ forums.Forum_1] $message(forum_name)
+Thread: $message(root_subject)\n\n"
+    if {$message(html_p)} {
+        append new_body "[ad_html_to_text -- $message(content)]"
+    } else {
+        append new_body $message(content)
+    }
+
+    append new_body "\n\n-- \n[ad_url][ad_conn package_url]message-view?[export_vars -anchor $message(message_id) [list [list message_id $message(root_message_id)]]]\n"
 
     return $new_body
 }
