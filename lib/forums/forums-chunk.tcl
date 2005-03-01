1d118556f55b08e5443de153f2df10c4f235807c
Jeff Davis <davis@xarg.net>
2005-03-01 00:01:37 +0000

    change ad_verify_and_get_user_id to ad_conn user_id, change ad_maybe_redirect_for_registration to auth::require_login, make some db_ and dt_ function -public

--- lib/forums/forums-chunk.tcl
+++ lib/forums/forums-chunk.tcl
@@ -9,7 +9,7 @@ ad_page_contract {
 }
 
 set package_id [ad_conn package_id]
-set user_id [ad_verify_and_get_user_id]
+set user_id [ad_conn user_id]
 
 set admin_p [permission::permission_p -object_id $package_id -privilege admin]
 
--- www/index.tcl
+++ www/index.tcl
@@ -9,7 +9,7 @@ ad_page_contract {
 }
 
 set package_id [ad_conn package_id]
-set user_id [ad_verify_and_get_user_id]
+set user_id [ad_conn user_id]
 set admin_p [permission::permission_p -party_id $user_id -object_id $package_id -privilege admin]
 set searchbox_p [parameter::get -parameter ForumsSearchBoxP -package_id $package_id -default 1]
 
