ec805c119202c8884e4a2d31a99d45a06ccede9e
Don Baccus <dhogaza@pacifier.com>
2008-03-19 22:21:46 +0000

    Cleaned up forums.js.  As discussed in e-mail, changed generic function names such as "toggle()" to "forums_toggle()" to reduce the odds of future name clashes causing as much confusion and embarrassment as the recent ones in zen and forums did.  Made formatting consistent.  Got rid of template::head calls to load unused javascript which just made the code more confusing.
    Behavior matches .LRN 2.2 behavior, the arrow on a message collapses or
    expands that message only, not its subtree.  Subtree would probably be better
    (and I swear it did, once upon a time) but that will have to wait for the
    future if people want it.

--- www/resources/forums.js
+++ www/resources/forums.js
@@ -1,52 +1,53 @@
-var collapse_symbol = '<img src="/resources/forums/Collapse16.gif" width="16" height="16" ALT="-" border="0">';
-var expand_symbol = '<img src="/resources/forums/Expand16.gif" width="16" height="16" ALT="+" border="0">';
-var replies = new Array();
+var forums_collapse_symbol = '<img src="/resources/forums/Collapse16.gif" width="16" height="16" ALT="-" border="0">';
+var forums_expand_symbol = '<img src="/resources/forums/Expand16.gif" width="16" height="16" ALT="+" border="0">';
+var forums_replies = new Array();
 
 // toggle visibility
 
-function getid(id) {
-       if(document.getElementById)
-		return document.getElementById(id);
-       else if(document.all)
-		return document.all(id);
-	return false;
+function forums_getid(id) {
+    if(document.getElementById)
+        return document.getElementById(id);
+    else if(document.all)
+        return document.all(id);
+    return false;
 }
 
-function toggle( targetId, state ){
-        var symbol;
-  		var content = getid( 'content'+targetId );
-  		var link = getid( 'toggle'+targetId );
-        if(!link || !content) return;
+function forums_toggle(targetId, state){
+    var symbol;
+    var content = forums_getid('content'+targetId);
+    var link = forums_getid('toggle'+targetId);
 
-        var s;
-    	if(state != null)
-	    	s = state;
-        else if (content.className == "dynexpanded") {
-            s = 0;
-	} else {
-            s = 1;
-	}
-        if (s) {
-            content.className = "dynexpanded";
-            symbol = collapse_symbol;
-        } else {
-            content.className = "dyncollapsed";
-            symbol = expand_symbol;
-        }
+    if (!link || !content) return;
 
-        if(link.innerHTML)
-            link.innerHTML = symbol;
-        else if(link.appendChild) {
-            while(link.hasChildNodes())
-                link.removeChild(link.firstChild);
-            link.appendChild(document.createTextNode(symbol));
-	    }
+    var s;
+    if (state != null)
+        s = state;
+    else if (content.className == "dynexpanded") {
+        s = 0;
+    } else {
+        s = 1;
+    }
+    if (s) {
+        content.className = "dynexpanded";
+        symbol = forums_collapse_symbol;
+    } else {
+        content.className = "dyncollapsed";
+        symbol = forums_expand_symbol;
+    }
+
+    if (link.innerHTML)
+        link.innerHTML = symbol;
+    else if (link.appendChild) {
+        while(link.hasChildNodes())
+            link.removeChild(link.firstChild);
+        link.appendChild(document.createTextNode(symbol));
+    }
 }
 
-function toggleList(a,state) {
-	if(!a.length) return;
-	for(var i = 0; i < a.length; ++i) {
-        toggle(a[i], state);
-	}
+function forums_toggleList(a,state) {
+    if (!a.length) return;
+    for (var i = 0; i < a.length; ++i) {
+        forums_toggle(a[i], state);
+    }
 }
  
