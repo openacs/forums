8b4cba6970a0b1de601c373342c5e20fab60e5b5
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2016-07-02 10:19:57 +0000

    - reduce dependencies on acs_object_party_privilege_map

--- tcl/forums-callback-procs.tcl
+++ tcl/forums-callback-procs.tcl
@@ -15,12 +15,14 @@ ad_proc -public -callback navigation::package_admin -impl forums {} {
 
     # Check for admin on the package...
     if {[permission::permission_p -object_id $package_id -privilege admin -party_id $user_id]} {
-        lappend actions [list LINK admin/ [_ acs-kernel.common_Administration] {} [_ forums.Admin_for_all]]
-
-        lappend actions [list LINK \
-                             [export_vars -base admin/permissions {{object_id $package_id}}] \
-                             [_ acs-kernel.common_Permissions] {} [_ forums.Permissions_for_all]]
-        lappend  actions [list LINK admin/forum-new [_ forums.Create_a_New_Forum] {} {}]
+        lappend actions \
+            [list LINK \
+                 admin/ \
+                 [_ acs-kernel.common_Administration] {} [_ forums.Admin_for_all]] \
+            [list LINK \
+                 [export_vars -base admin/permissions {{object_id $package_id}}] \
+                 [_ acs-kernel.common_Permissions] {} [_ forums.Permissions_for_all]] \
+            [list LINK admin/forum-new [_ forums.Create_a_New_Forum] {} {}]
     }
 
     # check for admin on the individual forums.
@@ -28,17 +30,18 @@ ad_proc -public -callback navigation::package_admin -impl forums {} {
         select forum_id, name, enabled_p
         from forums_forums
         where package_id = :package_id
-        and exists (select 1 from acs_object_party_privilege_map pm
-                    where pm.object_id = forum_id
-                    and pm.party_id = :user_id
-                    and pm.privilege = 'admin')
     } {
-        lappend actions [list SECTION "Forum $name ([ad_decode $enabled_p t [_ forums.enabled] [_ forums.disabled]])" {}]
+        if {[permission::permission_p -object_id $forum_id -privilege admin -party_id $user_id]} {
 
-        lappend actions [list LINK [export_vars -base admin/forum-edit forum_id] \
-                             [_ forums.Edit_forum_name] {} {}]
-        lappend actions [list LINK [export_vars -base admin/permissions {{object_id $forum_id} return_url}] \
-                             [_ forums.Permission_forum_name] {} {}]
+            lappend actions \
+                [list SECTION "Forum $name ([ad_decode $enabled_p t [_ forums.enabled] [_ forums.disabled]])" {}] \
+                [list LINK \
+                     [export_vars -base admin/forum-edit forum_id] \
+                     [_ forums.Edit_forum_name] {} {}] \
+                [list LINK \
+                     [export_vars -base admin/permissions {{object_id $forum_id} return_url}] \
+                     [_ forums.Permission_forum_name] {} {}]
+        }
     }
     return $actions
 }
